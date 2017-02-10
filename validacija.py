eccef1a211d08af0820cbf326a402b4d3474e93d
Luksa Kraljevic <kluksa@gmail.com>
2017-02-10 22:10:30 +0100

    Priprema za rebase

--- validacija.py
+++ validacija.py
@@ -3,18 +3,19 @@
 
 import sys
 
-from PyQt4 import QtGui
+import PyQt4
 
 from app.view import mainwindow
 
 
 def main(argv):
-    app = QtGui.QApplication(sys.argv)
+    app = PyQt4.QtGui.QApplication(argv)
     gui = mainwindow.MainWindow()
     gui.setWindowTitle('Validacija podataka')
     gui.showMaximized()
     gui.handle_login()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main(sys.argv)
